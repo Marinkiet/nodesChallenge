25f5b102e1456a5827a40fdb41fa84b0475f2fb8
Marinkie <tmarinkie12@gmail.com>
2024-10-04 04:33:27 +0200

    installing amplify

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MainBanner from "../components/banners/MainBanner";
 import '../Assets/Styles/Signin.scss'
+import '../Assets/Styles/SigninForm.scss'
 function Signin() {
   return (
     <div className="main-container">
@@ -9,15 +10,37 @@ function Signin() {
         <MainBanner />
       </div>
 
-      {/* Form Container */}
+      {/* Form Container - green*/}
       <div className="form-container">
-        <div>
-          {/* Sign-in form */}
+
+        <div className="signin-form"> {/* orange -1 */}
+          <div className="sigin-header-container mb-2">
+            {/* sigin-header-container */}
+            <h1 className="sigin-header">Sign in</h1>
+          </div>
+          <div className="form-inputs-container">
+            {/*form-inputs-container */}
+            <div className="mb-3">
+              <input type="email" className="form-control form-control-lg email-input"  placeholder="email.email@email.com" />
+            </div>
+            <div className="mb-3">
+              <input type="password" className="form-control form-control-lg password-input" value="email.email@email.com" />
+            </div>
+          </div>
+          <div className="sign-in-button-container">
+            {/* sign-in-button-container */}
+            <button type="button" className="btn btn-warning signin-button">Sign in</button>
+          </div>
         </div>
-        <div>
-          {/* Sign-in options */}
+
+        <div className="sigin-options">  {/* orange -2 */}
+          <div>
+            {/* Sign-in options */}
+          </div>
         </div>
+
       </div>
+
     </div>
   )
 }
